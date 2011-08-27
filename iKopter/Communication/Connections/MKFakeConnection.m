21be7e0ab83525ff59aa6154ca01b69601a52ba9
frank <frank@frankblumenberg.de>
2011-08-27 17:07:41 +0200

    Fixed OSD data loading for the fake connection

--- iKopter/Communication/Connections/MKFakeConnection.m
+++ iKopter/Communication/Connections/MKFakeConnection.m
@@ -280,17 +280,22 @@ static NSString * const MKDummyConnectionException = @"MKDummyConnectionExceptio
     NSError*  error;
     NSString* data = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:&error ];
     
-    self.osdData = [data csvRows];
     
-    dataRow = 0;
-    [self updateNaviDataFromRow:dataRow];
-    
-    osdTimer=[NSTimer scheduledTimerWithTimeInterval: 1.0 
-                                              target:self 
-                                            selector:@selector(nextRow) 
-                                            userInfo:nil 
-                                             repeats:YES];
-
+    dispatch_queue_t queue= dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,0);
+    dispatch_async(queue, ^{
+      self.osdData = [data csvRows];
+      
+      dispatch_async(dispatch_get_main_queue(), ^{
+        dataRow = 0;
+        [self updateNaviDataFromRow:dataRow];
+        
+        osdTimer=[NSTimer scheduledTimerWithTimeInterval: 1.0 
+                                                  target:self 
+                                                selector:@selector(nextRow) 
+                                                userInfo:nil 
+                                                 repeats:YES];
+      });
+    });
   }
   return self;
 }
