19e092041858f95ede2e0bcbbec24d3d930428cd
frank <frank@frankblumenberg.de>
2012-04-15 17:08:42 +0200

    Added an extra Version for the iPhone3G
    The different architectures didn't worked well together. So now it's brute force.

--- packCydiaPackage.sh
+++ packCydiaPackage.sh
@@ -1,5 +1,5 @@
 PROJECTMAIN=$(pwd)
-PROJECT_NAME=$(basename "${PROJECTMAIN}")
+PROJECT_NAME="iKopter"
 APPMAIN=${PROJECTMAIN}/Cydia/iKopter/Applications/${PROJECT_NAME}.app
 #
 if [[ -f "${APPMAIN}/Info.plist" ]]
@@ -17,6 +17,25 @@ echo -e "\"${buildPlist}\" does not contain key: \"CFBundleVersion\""
 exit 1
 fi
 
+PROJECT_NAME="iKopter3G"
+APPMAIN=${PROJECTMAIN}/Cydia/iKopter3G/Applications/${PROJECT_NAME}.app
+#
+if [[ -f "${APPMAIN}/Info.plist" ]]
+then
+buildPlist="${APPMAIN}/Info.plist"
+else
+echo -e "Can't find the plist: ${PROJECT_NAME}-Info.plist"
+exit 1
+fi
+#
+buildVersion=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${buildPlist}" 2>/dev/null)
+if [[ "${buildVersion}" = "" ]]
+then
+echo -e "\"${buildPlist}\" does not contain key: \"CFBundleVersion\""
+exit 1
+fi
+
+
 cd ${PROJECTMAIN}/Cydia/iKopter/DEBIAN
 sed 's/Version:.*$/Version: '${buildVersion}'/' control > control1
 echo "==============================="
@@ -28,7 +47,8 @@ mkdir ${PROJECTMAIN}/Cydia/repo
 mkdir ${PROJECTMAIN}/Cydia/repo/deb
 
 cd ${PROJECTMAIN}/Cydia
-/usr/local/bin/dpkg-deb -b iKopter ${PROJECTMAIN}/Cydia/repo/deb/${PROJECT_NAME}Package.deb
+/usr/local/bin/dpkg-deb -b iKopter ${PROJECTMAIN}/Cydia/repo/deb/iKopterPackage.deb
+/usr/local/bin/dpkg-deb -b iKopter3G ${PROJECTMAIN}/Cydia/repo/deb/iKopter3GPackage.deb
 
 cd ${PROJECTMAIN}/Cydia/repo 
 /usr/local/bin/dpkg-scanpackages deb / > Packages
